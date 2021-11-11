e2376f1afa03b3e402f7063fc76443e4a5491001
Tiep Cao <lanhtudongxanh@gmail.com>
2021-11-11 17:35:27 +0700

    Hello world Unit 1

---
+++ Unit_1/.Unit_1.mod.cmd
@@ -0,0 +1,1 @@
+cmd_/home/vkernel/Documents/Kernel_Course/Unit_1/Unit_1.mod := { echo  /home/vkernel/Documents/Kernel_Course/Unit_1/Unit_1.o;  echo; } > /home/vkernel/Documents/Kernel_Course/Unit_1/Unit_1.mod
---
+++ Unit_1/Unit_1.mod
@@ -0,0 +1,2 @@
+/home/vkernel/Documents/Kernel_Course/Unit_1/Unit_1.o
+
